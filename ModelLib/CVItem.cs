2d2300c310ad2a4ed9f31c75892ef94c4e41098e
Thomas (Noit) <tba@live.dk>
2021-05-03 15:12:52 +0200

    Add project files.

---
+++ ModelLib/CVItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    public class CVItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public Category Category { get; set; }
+        public string DateFromString { get; set; }
+        public string DateToString { get; set; }
+    }
+}
