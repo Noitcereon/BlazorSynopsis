f4159928112421805688e1d2cd68bb1d4e9ad31d
Thomas (Noit) <tba@live.dk>
2021-05-20 15:10:47 +0200

    added a few event listeners.

--- BlazorPWA/wwwroot/service-worker.js
+++ BlazorPWA/wwwroot/service-worker.js
@@ -1,4 +1,23 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+console.log({ self });
+
+self.addEventListener('install', (e) => {
+    console.log("Service Worker: installed");
+})
+
+self.addEventListener('activated', (e) => {
+    console.log("Service Worker: activated");
+    // waitUntil is an ExtendableEvent (event lifespan extension)
+    //e.waitUntil(
+    //    caches.keys().then(cacheNames => {
+    //        return Promise.all()
+    //    }) 
+    //)
+})
+
+self.addEventListener('fetch', (e) => {
+    console.log("Service Worker: intercepted a fetch call");
+    console.log(e.request);
+})
